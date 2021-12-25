eeb30ef4991c5a31c9bd019c326c6d449689dffa
Moamen Soroor <moamensoroor@gmail.com>
2021-12-25 14:39:07 +0200

    more cleanning

--- SalesManProblem.Algorithms/Algorithm/GNA/GNAConfigurations.cs
+++ SalesManProblem.Algorithms/Algorithm/GNA/GNAConfigurations.cs
@@ -7,7 +7,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SalesManProblem.Algorithms.Utils;
 using SalesManProblem.Algorithms.Configurations;
 
 namespace SalesManProblem.Algorithms.GNA
--- SalesManProblem.Algorithms/Extensions.cs
+++ SalesManProblem.Algorithms/Extensions.cs
@@ -4,19 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SalesManProblem.Algorithms
+namespace SalesManProblem
 {
-    
-    public static class LinqExtensions
+    public static class Extensions
     {
 
-        public static void DoWhile<T>(Func<T,int,bool> predicate,Action<int> body)
+        /// <summary>
+        /// a Composer Higher order function
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <param name="f1"></param>
+        /// <param name="f2"></param>
+        /// <returns></returns>
+        public static Func<T1, T3> Compose<T1, T2, T3>(this Func<T1, T2> f1, Func<T2, T3> f2)
         {
-            //int round = 0;
-            //do
-            //{
-            //    body(round);
-            //} while (predicate());
+            return (value) => f2(f1(value));
         }
+
+
+        /// <summary>
+        /// adapter higher order function that is used reduce that parameters of a function
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <param name="f1"></param>
+        /// <param name="t2"></param>
+        /// <returns></returns>
+        public static Func<T1, T3> ReduceParameters<T1, T2, T3>(this Func<T1, T2, T3> f1, T2 t2)
+        {
+            return (t1) => f1(t1, t2);
+        }
+
+        /// <summary>
+        /// adapter higher order function that is used reduce that parameters of a function
+        /// </summary>
+        /// <typeparam name="T1"></typeparam>
+        /// <typeparam name="T2"></typeparam>
+        /// <typeparam name="T3"></typeparam>
+        /// <typeparam name="T4"></typeparam>
+        /// <param name="f1"></param>
+        /// <param name="t3"></param>
+        /// <returns></returns>
+        public static Func<T1, T2, T4> ReduceParameters<T1, T2, T3, T4>(this Func<T1, T2, T3, T4> f1, T3 t3)
+        {
+            return (t1, t2) => f1(t1, t2, t3);
+        }
+
+
     }
 }
